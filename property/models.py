3bf39830aac7eeb78f4aa053bbc55500b8a18ae7
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-06-23 14:55:45 +0300

    Add db index to Owner fields

--- property/models.py
+++ property/models.py
@@ -84,18 +84,22 @@ class Flat(models.Model):
 class Owner(models.Model):
     owner = models.CharField(
         'ФИО владельца',
-        max_length=200)
+        max_length=200,
+        db_index=True)
     owners_phonenumber = models.CharField(
         'Номер владельца',
-        max_length=20)
+        max_length=20,
+        db_index=True)
     owner_pure_phone = PhoneNumberField(
         'Нормализованный номер владельца',
         blank=True,
-        null=True)
+        null=True,
+        db_index=True)
     owner_flats = models.ManyToManyField(
         Flat,
         related_name='owners',
-        verbose_name='Квартиры в собственности')
+        verbose_name='Квартиры в собственности',
+        db_index=True)
 
     def __str__(self):
         return f'{self.owner}, тел. {self.owner_pure_phone}'
