1481f49b0fb8bf540f834f293f356ab1fddc7560
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-06-23 12:16:06 +0300

    Add db model for owners

--- property/models.py
+++ property/models.py
@@ -14,8 +14,7 @@ class Flat(models.Model):
     owner_pure_phone = PhoneNumberField(
         'Нормализованный номер владельца',
         blank=True,
-        null=True,
-        )
+        null=True)
     created_at = models.DateTimeField(
         'Когда создано объявление',
         default=timezone.now,
@@ -80,6 +79,26 @@ class Flat(models.Model):
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
+
+
+class Owner(models.Model):
+    owner = models.CharField(
+        'ФИО владельца',
+        max_length=200)
+    owners_phonenumber = models.CharField(
+        'Номер владельца',
+        max_length=20)
+    owner_pure_phone = PhoneNumberField(
+        'Нормализованный номер владельца',
+        blank=True,
+        null=True)
+    owner_flats = models.ManyToManyField(
+        Flat,
+        related_name='owners',
+        verbose_name='Квартиры в собственности')
+
+    def __str__(self):
+        return f'{self.owner}, тел. {self.owner_pure_phone}'
 
 
 class Complaint(models.Model):
