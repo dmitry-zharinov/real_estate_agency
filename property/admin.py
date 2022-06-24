d5c537833d825600dba4c8178c8650cd4c5d8277
Dmitriy Zharinov <zharinovdm@yandex.ru>
2022-06-24 22:01:21 +0300

    Add usage of decorators

--- property/admin.py
+++ property/admin.py
@@ -7,7 +7,7 @@ class OwnerInline(admin.TabularInline):
     model = Owner.flats.through
     raw_id_fields = ['owner']
 
-
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address']
     readonly_fields = ['created_at']
@@ -21,15 +21,11 @@ class FlatAdmin(admin.ModelAdmin):
     raw_id_fields = ['liked_by']
     inlines = [OwnerInline]
 
-
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ['user', 'flat']
 
-
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ['flats']
 
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
